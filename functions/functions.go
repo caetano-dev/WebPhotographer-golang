bdf322bc677bf9fad97db246c3117ba69ca8f5dc
drull1000 <pcaetanop@protonmail.com>
2021-12-31 19:30:13 -0300

    Link based

--- functions/functions.go
+++ functions/functions.go
@@ -1,14 +1,11 @@
 package funtions
 
 import (
-	"context"
-	"io/ioutil"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
 
-	"github.com/chromedp/cdproto/page"
-	"github.com/chromedp/chromedp"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
@@ -26,44 +23,24 @@ func Screenshot(update tgbotapi.Update) {
 		panic(error)
 	}
 
-	ctx, cancel := chromedp.NewContext(context.Background(), chromedp.WithDebugf(log.Printf))
-	defer cancel()
+	website := update.Message.Text
 
-	url := update.Message.Text
-
-	if !strings.Contains(url, "http") {
-		url = "http://" + url
+	if !strings.Contains(website, "http") {
+		website = "http://" + website
 	}
 
-	if !strings.Contains(url, ".") {
-		url = url + ".com"
+	if !strings.Contains(website, ".") {
+		website = website + ".com"
 	}
 
-	filename := "screenshot.png"
+	url := "https://api.screenshotmachine.com/?key=" + getRandomKey() + "&url=" + website + "&dimension=1024x3080"
 
-	var imageBuf []byte
-	if error := chromedp.Run(ctx, screenshotTasks(url, &imageBuf)); error != nil {
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Website not found.")
-		bot.Send(msg)
-	}
-
-	if error := ioutil.WriteFile(filename, imageBuf, 0644); error != nil {
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "An error happened, please try again.")
-		bot.Send(msg)
-	}
-
-	photo := tgbotapi.NewInputMediaPhoto(tgbotapi.FilePath(filename))
-	bot.SendMediaGroup(tgbotapi.NewMediaGroup(update.Message.Chat.ID, []interface{}{photo}))
-
+	screenshot := tgbotapi.NewMessage(update.Message.Chat.ID, url)
+	bot.Send(screenshot)
 }
 
-//screenshotTasks will navigate to the website and take a screenshot of it.
-func screenshotTasks(url string, imageBuf *[]byte) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Navigate(url),
-		chromedp.ActionFunc(func(ctx context.Context) (error error) {
-			*imageBuf, error = page.CaptureScreenshot().WithQuality(90).Do(ctx)
-			return error
-		}),
-	}
+//get getRandomKey will return a random item from a slice.
+func getRandomKey() string {
+	items := []string{"9cf822", "764f21", "6244bf ", "97ec9a", "80e53c", "f2909b", "7aaf56", "e7f994"}
+	return items[rand.Intn(len(items))]
 }

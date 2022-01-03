11e43142b68b9cb67643b5d193d596f3aec0adab
drull1000 <pcaetanop@protonmail.com>
2022-01-03 15:55:52 -0300

    Send downloaded image instead of link

--- functions/functions.go
+++ functions/functions.go
@@ -2,15 +2,26 @@ package funtions
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
 	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
+
+var (
+	url string
+)
+
+// Keys struct will hold the keys.
+type Keys struct {
+	Keys []string `json:"keys"`
+}
 
 //Screenshot function will save the screenshot and send it.
 func Screenshot(update tgbotapi.Update) {
@@ -21,9 +32,7 @@ func Screenshot(update tgbotapi.Update) {
 
 	bot, error := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
 
-	if error != nil {
-		panic(error)
-	}
+	checkError(error)
 
 	website := update.Message.Text
 
@@ -35,28 +44,24 @@ func Screenshot(update tgbotapi.Update) {
 		website = website + ".com"
 	}
 
-	url := "https://api.screenshotmachine.com/?key=" + randomKey() + "&url=" + website + "&dimension=1024x3080"
+	url = "https://api.screenshotmachine.com/?key=" + randomKey() + "&url=" + website + "&dimension=1024x3080"
+	file, err := os.Create("screenshot.png")
 
-	screenshot := tgbotapi.NewMessage(update.Message.Chat.ID, url)
-	bot.Send(screenshot)
-}
+	checkError(err)
 
-// Keys struct will hold the keys.
-type Keys struct {
-	Keys []string `json:"keys"`
+	putFile(file, httpClient())
+
+	photo := tgbotapi.NewInputMediaPhoto(tgbotapi.FilePath("./screenshot.png"))
+	bot.SendMediaGroup(tgbotapi.NewMediaGroup(update.Message.Chat.ID, []interface{}{photo}))
 }
 
 // randomKey will return a random item from a slice.
 func randomKey() string {
 	file, error := os.Open("keys.json")
-	if error != nil {
-		log.Fatal(error)
-	}
+	checkError(error)
 
 	keys, error := ioutil.ReadAll(file)
-	if error != nil {
-		log.Fatal(error)
-	}
+	checkError(error)
 
 	var keysStruct Keys
 	json.Unmarshal(keys, &keysStruct)
@@ -65,4 +70,34 @@ func randomKey() string {
 
 	return randomKey
 
+}
+func putFile(file *os.File, client *http.Client) {
+	resp, err := client.Get(url)
+
+	checkError(err)
+
+	defer resp.Body.Close()
+
+	io.Copy(file, resp.Body)
+
+	defer file.Close()
+
+	checkError(err)
+}
+
+func httpClient() *http.Client {
+	client := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			r.URL.Opaque = r.URL.Path
+			return nil
+		},
+	}
+
+	return &client
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
